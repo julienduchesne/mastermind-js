5b60d79c2748c7719af60e7401147f0655652fb0
Julien Duchesne <julienduchesne@live.com>
2020-10-20 21:19:32 -0400

    Finish config page with circle count

--- src/scenes/config.js
+++ src/scenes/config.js
@@ -16,10 +16,10 @@ export default class ConfigScene extends Phaser.Scene {
         return this.add.text(x, y, text).setColor(PALETTE.medium).setFontSize(30).setFontFamily("Bangers").setPadding(5, 5).setOrigin(0, 0.5);
     }
 
-    drawCircles(count, x, y, width) {
+    drawCircles(count, x, y, width, colors = true) {
         var group = this.add.group();
         for (var i = 0; i < count; i++) {
-            var color = Phaser.Display.Color.HexStringToColor(CIRCLE_COLORS[i]).color;
+            var color = Phaser.Display.Color.HexStringToColor(colors ? CIRCLE_COLORS[i] : toHex("white")).color;
             group.add(this.add.circle(x + i * 20, y, 20, color).setStrokeStyle(2, toHex("black")).setAlpha(1));
         }
         x += (width / count) / 2;
@@ -29,31 +29,37 @@ export default class ConfigScene extends Phaser.Scene {
         return group;
     }
 
+    addCountConfig(y, text, defaultValue, min, max, coloredCircled) {
+        // Number of colors
+        this.configText(150, y, text);
+        var countText = this.configText(650, y, (defaultValue - 1).toString());
+        var circles = null;
+        var getChangeColorCount = function (change) {
+            return function () {
+                var newCount = Math.max(min, Math.min(parseInt(countText.text) + change, max));
+                countText.setText(newCount);
+                if (circles) {
+                    circles.destroy(true);
+                }
+                circles = this.scene.drawCircles(newCount, this.scene.cameras.main.width / 2, y + 75, newCount * 50, coloredCircled);
+            }
+        }
+        this.configText(600, y, '◀').setInteractive().on("pointerdown", getChangeColorCount(-1));
+        this.configText(700, y, '▶').setInteractive().on("pointerdown", getChangeColorCount(+1)).emit("pointerdown");
+
+        return countText
+    }
+
     preload() { }
     create() {
         var halfWidth = this.cameras.main.width / 2;
         this.add.text(halfWidth, 100, 'Configuration').setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10);
 
-        // Number of colors
-        this.configText(150, 200, 'Number of Colors:');
-        var colorCountText = this.configText(650, 200, '4');
-        var circles = null;
-        var getChangeColorCount = function (change) {
-            return function () {
-                var newCount = Math.max(2, Math.min(parseInt(colorCountText.text) + change, 12));
-                colorCountText.setText(newCount);
-                if (circles) {
-                    circles.destroy(true);
-                }
-                circles = this.scene.drawCircles(newCount, this.scene.cameras.main.width / 2, 275, newCount * 25);
-            }
-        }
-        this.configText(600, 200, '◀').setInteractive().on("pointerdown", getChangeColorCount(-1));
-        this.configText(700, 200, '▶').setInteractive().on("pointerdown", getChangeColorCount(+1)).emit("pointerdown");
-
+        var colorCountText = this.addCountConfig(200, 'Number of Possible Colors:', 6, 2, 12, true)
+        var circleCountText = this.addCountConfig(400, 'Number of Circles to Guess:', 4, 3, 6, false)
 
         this.add.text(halfWidth, 800, 'START').setFontSize(60).setPadding(10, 10).setFontFamily("Bangers").setColor(PALETTE.dark).setOrigin(0.5).setInteractive().on('pointerdown', function () {
-            this.scene.scene.start("GameScene", {})
+            this.scene.scene.start("GameScene", { colorCount: parseInt(colorCountText.text), circleCount: parseInt(circleCountText.text) })
         });
     }
 
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -10,7 +10,10 @@ export default class GameScene extends Phaser.Scene {
 
     preload() { }
     create() {
+        console.log(this)
         var halfWidth = this.cameras.main.width / 2;
-        this.add.text(halfWidth, 100, 'The Game').setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10);
+        this.add.text(halfWidth, 100, 'The Game [WIP]').setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10);
+        this.add.text(halfWidth, 200, 'Color count: ' + this.scene.settings.data["colorCount"]).setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10);
+        this.add.text(halfWidth, 300, 'Circle count: ' + this.scene.settings.data["circleCount"]).setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10);
     }
 }
