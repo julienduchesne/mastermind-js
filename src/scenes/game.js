e44e2d614887d24b4d4422a9e16b996ea0790242
Julien Duchesne <julienduchesne@live.com>
2020-10-25 22:13:28 -0400

    Add confirm check mark and row numbers

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -7,6 +7,7 @@ import Anchor from 'phaser3-rex-plugins/plugins/anchor.js';
 export default class GameScene extends Phaser.Scene {
     constructor() {
         super('GameScene');
+        this.currentRow = 0;
     }
 
     preload() { }
@@ -14,40 +15,82 @@ export default class GameScene extends Phaser.Scene {
         var colorCount = this.scene.settings.data["colorCount"];
         var circleCount = this.scene.settings.data["circleCount"];
         new Anchor(
-            this.add.text(0, 0, "Restart").setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10)
+            this.add.text(0, 0, "Restart").setColor(PALETTE.dark).setFontSize(52).setFontFamily("Bangers").setPadding(10, 10)
                 .setInteractive().on('pointerdown', function () { this.scene.scene.start("ConfigScene", {}) }),
             { left: "left+50", top: "top+10" }
         );
         new Anchor(
-            this.add.text(0, 0, "Guess for me").setColor(PALETTE.dark).setFontSize(52).setOrigin(0.5).setFontFamily("Bangers").setPadding(10, 10)
+            this.add.text(0, 0, "Guess for me").setColor(PALETTE.dark).setFontSize(52).setFontFamily("Bangers").setPadding(10, 10)
                 .setInteractive().on('pointerdown', function () { alert("Not implemented") }),
             { right: "right-50", top: "top+10" }
         );
 
-        var spaceBetweenCircles = 20;
+        var spaceBetweenItems = 20;
         var spaceBetweenLines = 40;
-        var circleRadius = 20;
-        var rowHeight = 2 * spaceBetweenCircles + 2 * circleRadius;
+        var itemWidth = 40;
+        var circleRadius = itemWidth / 2;
+        var rowHeight = 2 * spaceBetweenItems + 2 * circleRadius;
         var numberOfInitialRows = Math.floor(((this.cameras.main.height - 100) / rowHeight));
 
         function CreateGrid(scene, colCount, rowCount) {
             var sizer = scene.rexUI.add.fixWidthSizer({
-                width: (2 * circleRadius + spaceBetweenCircles) * circleCount + 120,
+                width: (itemWidth + spaceBetweenItems) * circleCount + 120 + (itemWidth + 20) * 2,
                 orientation: 0,
                 space: {
                     left: 0, right: 0, top: 0, bottom: 0,
-                    item: spaceBetweenCircles, line: spaceBetweenLines
+                    item: spaceBetweenItems, line: spaceBetweenLines
                 },
             });
 
-            for (var i = 0; i < rowCount; i++) {
+            for (var i = rowCount - 1; i >= 0; i--) {
+                sizer.add(scene.rexUI.add.label({
+                    width: itemWidth,
+                    height: itemWidth,
+                    background: scene.add.circle(0, 0, circleRadius, Phaser.Display.Color.HexStringToColor(PALETTE.background).color),
+                    text: scene.add.text(0, 0, `${i + 1}`, {
+                        color: PALETTE.medium,
+                        fontSize: circleRadius * 1.5,
+                        fontFamily: "Bangers",
+                        padding: {
+                            left: 5,
+                            right: 5,
+                            top: 5,
+                            bottom: 5,
+                        },
+                    }),
+                    align: "center"
+                }));
                 for (var j = 0; j < colCount; j++) {
                     sizer.add(
                         scene.add.circle(0, 0, circleRadius, Phaser.Display.Color.HexStringToColor(PALETTE.emptyCirle).color).setStrokeStyle(1, PALETTE.dark)
                     );
                 }
+                if (scene.currentRow == i) {
+                    sizer.add(scene.rexUI.add.label({
+                        width: itemWidth,
+                        height: itemWidth,
+                        background: scene.add.circle(0, 0, circleRadius, Phaser.Display.Color.HexStringToColor(PALETTE.background).color),
+                        text: scene.add.text(0, 0, "✔️", {
+                            color: PALETTE.medium,
+                            fontSize: circleRadius,
+                            fontFamily: "Bangers",
+                            padding: {
+                                left: 5,
+                                right: 5,
+                                top: 5,
+                                bottom: 5,
+                            },
+                        }),
+                        align: "center"
+                    }));
+                } else {
+                    sizer.add(
+                        scene.add.circle(0, 0, circleRadius, Phaser.Display.Color.HexStringToColor(PALETTE.background).color)
+                    );
+                }
+
                 sizer.add(
-                    scene.rexUI.add.roundRectangle(0, 0, 100, circleRadius * 2, 10, Phaser.Display.Color.HexStringToColor(PALETTE.light).color)
+                    scene.rexUI.add.roundRectangle(0, 0, 100, itemWidth, 10, Phaser.Display.Color.HexStringToColor(PALETTE.light).color)
                 );
             }
             return sizer
